4d6b52eb167114eccac3c0d6127663e5abcca218
Mitchell Philipp <mgpalpha1@gmail.com>
2021-08-05 19:08:18 -0400

    add arrow gizmos

--- Assets/CustomGizmos.cs
+++ Assets/CustomGizmos.cs
@@ -15,8 +15,22 @@ public static class CustomGizmos {
             } else {
                 o = ((Component) target).gameObject;
             }
-            Gizmos.DrawLine(origin, o.transform.position);
+            DrawArrow(origin, o.transform.position);
         }
         Gizmos.color = oldColor;
+    }
+
+    public static void DrawArrow(Vector3 from, Vector3 to, float arrowHeadLength = 0.4f, float arrowHeadAngle = 30.0f)
+    {
+        Vector3 direction = to - from;
+
+        Gizmos.DrawRay(from, direction);
+       
+        // Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0,0,180+arrowHeadAngle) * new Vector3(0,0,1);
+        // Vector3 left = Quaternion.LookRotation(direction) * Quaternion.Euler(0,0,180-arrowHeadAngle) * new Vector3(0,0,1);
+        Vector3 right = Quaternion.Euler(0,0,180+arrowHeadAngle) * direction.normalized;
+        Vector3 left = Quaternion.Euler(0,0,180-arrowHeadAngle) * direction.normalized;
+        Gizmos.DrawRay(from + direction, right * arrowHeadLength);
+        Gizmos.DrawRay(from + direction, left * arrowHeadLength);
     }
 }
