8f347dcbbb5278ad7958c624a57c78e1e387c1c3
Mitchell Philipp <mgpalpha1@gmail.com>
2021-09-22 01:00:51 -0400

    intentionally bad cs script

---
+++ Assets/Scripts/TestBad.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        This Shouldn't Compile
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
