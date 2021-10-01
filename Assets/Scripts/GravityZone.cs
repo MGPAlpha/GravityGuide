897e1a2f82b013bc23c7a7842441e6336a649571
Mitchell Philipp <mgpalpha1@gmail.com>
2021-09-30 23:08:17 -0400

    able to turn gravity zones on and off

--- Assets/Scripts/GravityZone.cs
+++ Assets/Scripts/GravityZone.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class GravityZone : MonoBehaviour
 {
+    [SerializeField] private bool startOn = true;
+    
     [SerializeField] private LayerMask affectedLayers;
     [SerializeField] private Vector2 gravityDir = Vector2.down;
+
+    [SerializeField] private float gravityFadeTime = .5f;
+
+    private bool gravityOn;
+    private float gravityShaderProgress = 1;
     
     private SpriteRenderer sp;
 
@@ -13,23 +20,44 @@ public class GravityZone : MonoBehaviour
     void Start()
     {
         sp = GetComponent<SpriteRenderer>();
+        gravityOn = startOn;
+        sp.material.SetFloat("_GravityOn", gravityOn ? 1 : 0);
+        gravityShaderProgress = gravityOn ? 1 : 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        gravityShaderProgress = Mathf.MoveTowards(gravityShaderProgress, gravityOn ? 1 : 0, Time.deltaTime / gravityFadeTime);
         float arrowDir = Vector2.Angle(gravityDir, Vector2.down);
         if (gravityDir.x > 0) arrowDir = 360 - arrowDir;
         sp.material.SetFloat("_ArrowAngle", arrowDir * 3.14f / 180);
+        sp.material.SetFloat("_GravityOn", gravityShaderProgress);
     }
     
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (affectedLayers == (affectedLayers | (1 << other.gameObject.layer))) {
+        if (gravityOn && (affectedLayers == (affectedLayers | (1 << other.gameObject.layer)))) {
             GravityObject g = other.GetComponent<GravityObject>();
             float oldGravity = g.personalGravity.magnitude;
             Vector2 newGravity = oldGravity * gravityDir.normalized;
             g.personalGravity = newGravity;
         }
+    }
+
+    public void SetOn(bool on) {
+        gravityOn = on;
+    }
+
+    public void TurnOn() {
+        SetOn(true);
+    }
+
+    public void TurnOff() {
+        SetOn(false);
+    }
+
+    public void ToggleOn() {
+        SetOn(!gravityOn);
     }
 }
