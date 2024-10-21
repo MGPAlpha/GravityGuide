d362108a5b5beec5f75076198476d641f1a8aa55
Mitchell Philipp <mgpalpha1@gmail.com>
2024-10-21 16:46:08 -0400

    integrate cinemachine and make zoom in on screen

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,30 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
-
-    private float defaultCameraZ;
 
     [SerializeField]
     [Range(0,10)]
     private float cameraStiffness;
 
-    [SerializeField]
-    private Transform playerTransform;
-    
+    private CinemachineCameraOffset cameraOffset;
+
     // Start is called before the first frame update
     void Start()
     {
-        defaultCameraZ = transform.position.z;
+        cameraOffset = GetComponent<CinemachineCameraOffset>();
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        transform.position = playerTransform.position - (playerTransform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition)) / (cameraStiffness + 2);
-        transform.position = Vector3.ProjectOnPlane(transform.position, Vector3.back);
-        transform.position += Vector3.forward * defaultCameraZ;
+        Vector2 offset = -(transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition)) / (cameraStiffness + 2);
+        // transform.position = playerTransform.position - (playerTransform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition)) / (cameraStiffness + 2);
+        // transform.position = Vector3.ProjectOnPlane(transform.position, Vector3.back);
+        // transform.position += Vector3.forward * defaultCameraZ;
+        cameraOffset.m_Offset = offset;
     }
 }
